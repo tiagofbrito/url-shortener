b93c943d41708179bb474744e2fb7bf0755a4cf6
tiago <tfb.am1@gmail.com>
2021-06-05 21:20:13 -0400

    feat: install bodyparser

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const PORT = 9999;
+
 const app = express();
+app.use(bodyParser.json());
 
+// TODO: add new route POST /name { "name": "JP" }, that prints in the server the name, and responds with { "message": "hello JP" }
 
-app.get('/hello', function(req, res) {
+app.post('/hello', function(req, res) {
   res.json({ message: 'hello world!' });
 });
 
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
 });
-
