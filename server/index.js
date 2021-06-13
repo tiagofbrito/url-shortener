e977e9f1fee387b243bf3dc2fc97fe3ec8ba2e2e
tiago <tfb.am1@gmail.com>
2021-06-13 18:45:32 -0400

    feat: new shortener link route

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const PORT = 9999;
 const app = express();
 app.use(bodyParser.json());
 
+app.post ('/links', function (req, res) {
+  console.log(req.body.link); 
+});
+
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
-})
+});
