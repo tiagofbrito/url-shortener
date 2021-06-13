8eb79889d30124ead1170a16b5907c7a8af2eb24
tiago <tfb.am1@gmail.com>
2021-06-13 18:20:03 -0400

    feat: remove old code

--- server/index.js
+++ server/index.js
@@ -6,33 +6,6 @@ const PORT = 9999;
 const app = express();
 app.use(bodyParser.json());
 
-// TODO: add new route POST /name { "name": "JP" }, that prints in the server the name, and responds with { "message": "hello JP" }
-
-app.get('/hello', function(req, res)  {
-  res.json({ message: 'hello world!' })
-});
-
-app.post('/name', function(req, res)  {
-  console.log(req.body);
-  //res.json({ message: 'Hello ' + req.body.name + ' you have ' + req.body.age + ' years old!' })
-
-  if (
-    typeof(req.body.name) !== 'string'
-  ) {
-    res.status(400).json ({error: 'erro'});
-    return;
-  }
- 
-  if (
-    typeof(req.body.age) !== 'number'
-  ) {
-    res.status(400).json ({error: 'digite os valores corretos, maluco!'});
-    return;
-  }
-  res.json({ message: 'Hello ' + req.body.name + ' you have ' + req.body.age + ' years old!' })
-
-});
-
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
 })
